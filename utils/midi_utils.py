256b3996579e5e4298bcaf495a2c139dcd592d4b
sikora-toma <ts50570@fer.hr>
2025-08-16 19:05:36 +0200

    Add functionality to return melody as tensor instead of a Melody instance

--- utils/midi_utils.py
+++ utils/midi_utils.py
@@ -1,21 +1,25 @@
 import mido
+import torch
 
-from melody_helpers import Melody, keys
+from utils.melody_helpers import Melody, keys
 
 
 
-def read_midi_file(filename, verbose=False):
+def read_midi_file(filename, return_tensor=False, verbose=False):
     mid = mido.MidiFile(filename)
 
 
     if verbose:
         print(f"Midi file with {len(mid.tracks)} tracks.")
+    notes = []
+    durations = []
+ 
     for i, track in enumerate(mid.tracks):
-        if 'Track' in track.name:
+        # this used to work, but dalmacija dataset has weird track naming conventions
+        # if 'Track' in track.name or 'Voice' in track.name:
+        if len(track.name)>0:
 
-            notes = []
-            durations = []
-            
+           
             for msg in track:
                 if msg.type=='note_on':
                     notes.append(msg.note)
@@ -30,9 +34,13 @@ def read_midi_file(filename, verbose=False):
                     key = keys[msg.key]
                     if verbose:
                         print(f"Key: {key}")
+ 
+    if return_tensor:
+        return torch.tensor([notes, durations], dtype=torch.float16)
+   
     return Melody(key=key, notes=notes, durations=durations)
 
 
 if __name__=="__main__":
-    melody = read_midi_file('../res/Free-trial-Moje-prve-222-pjesme-17-pdf.mid')
+    melody = read_midi_file('../res/zvira voda.mid')
     print(melody)
